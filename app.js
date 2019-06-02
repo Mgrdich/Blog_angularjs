69be96a68ecc3cd8b60f706be7ad5cb42265ce6e
Mgrdich <mgotm13@gmail.com>
2019-06-02 15:40:49 +0400

    Some additional work on controllers filters

--- app.js
+++ app.js
@@ -9,6 +9,37 @@ app.run(function () { /*when it runs*/
 });
 
 
-app.controller('myController', ['$scope',function (scope) {
+app.controller('myController', ['$scope', function (scope) {
+
     scope.name = "mgo";
+    scope.ninjas = ['yoshi', 'crystal', 'ryu', 'shaun'];
+
+    scope.ninjasObj = [
+        {
+            name: "Yoshi",
+            belt: "black",
+            available:true
+        },
+        {
+            name: "Crystal",
+            belt: "yellow",
+            available:true
+        },
+        {
+            name: "Shaun",
+            belt: "black",
+            available:false
+        },
+        {
+            name: "Ryu",
+            belt: "red",
+            available:true
+        }
+    ];
+
+    scope.FilterArray = function (filtered) {
+        scope.ninjasObj = scope.ninjasObj.filter(function (iter) {
+            return iter.name !== filtered;
+        });
+    };
 }]);
